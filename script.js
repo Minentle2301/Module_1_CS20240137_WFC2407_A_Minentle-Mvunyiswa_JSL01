e4bde6eb972e4697bbdc13cdec1b767e65ae681d
Minentle Mvunyiswa <102150619+Minentle2301@users.noreply.github.com>
2024-08-30 11:57:32 +0200

    added the slice method

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function validateSyntax() {
     
     
       // Check if input starts with 'pet_' and followed by alphanumeric characters
-      if (input.startsWith('pet_') && /^[a-zA-Z0-9]+$/.test(input.substring(4))) {
+      if (input.startsWith('pet_') && /^[a-zA-Z0-9]+$/.test(input.slice(4))) {
         result = "valid"; // Set result to valid
     } else {
         result = "syntax invalid"; // Set result to invalid
